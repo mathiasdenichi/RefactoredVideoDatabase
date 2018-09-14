0403c47bd0b47848f97239aeda3427ab1a124913
JulepSinclair <42009732+JulepSinclair@users.noreply.github.com>
2018-09-14 15:12:58 -0400

    Inital Commit for refactoring

---
+++ src/components/elements/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+        <h1> "You silly, that page dosen't exist!" XD </h1>
+        </div>
+    )
+}
+
+export default NotFound;
